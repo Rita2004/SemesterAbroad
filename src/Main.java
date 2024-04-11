b84e7b0ffab343b00084deee26409158f7f2d882
Rita2004 <129674873+Rita2004@users.noreply.github.com>
2024-04-12 00:03:23 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
+import javax.swing.*;
 
 public class Main {
-
     public static void main(String[] args) {
-        // Create destinations
-        List<Destination> destinations = createDestinations();
-
-        // Create the server
-        Server server = new Server(destinations);
-
-        // Connect GUI to the server
-        ClientGUI clientGUI = new ClientGUI(server);
-
-        // Run the server
-        server.run();
-    }
-
-    private static List<Destination> createDestinations() {
-        List<Destination> destinations = new ArrayList<>();
-        for (String university : Destination.UNIVERSITY_LIST) {
-            // You can randomly choose maxStudents between 1 and 5
-            int maxStudents = (int) (Math.random() * 5) + 1;
-            Destination destination = new Destination(university, maxStudents);
-            destinations.add(destination);
-        }
-        return destinations;
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                ClientGUI clientGUI = new ClientGUI();
+                clientGUI.setSize(500, 300);
+                clientGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                clientGUI.setVisible(true);
+            }
+        });
     }
 }
