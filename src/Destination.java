e85675b04a7ce2ca303db22244d7783e62c2ecd9
Rita2004 <129674873+Rita2004@users.noreply.github.com>
2024-04-19 22:16:12 +0400

    Update Destination.java

--- src/Destination.java
+++ src/Destination.java
@@ -1,37 +1,100 @@
-public class Destination {
-    private int id; 
-    private String name;
-    private int maxCapacity;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-    // Constructor
-    public Destination(int id, String name, int maxCapacity) {
+public class Destination implements Serializable {
+    private int id;
+    private String name;
+    private int maxStudents;
+    private List<Student> assignedStudents;
+
+    public Destination(int id, String name, int maxStudents) {
         this.id = id;
         this.name = name;
-        this.maxCapacity = maxCapacity;
+        this.maxStudents = maxStudents;
+        this.assignedStudents = new ArrayList<>();
     }
 
-    // Getters and Setters
-    public int getId() {
+    boolean removeStudent(Student student){
+        for(Student s: assignedStudents){
+            if(s==student){
+                assignedStudents.remove(student);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean addStudent(Student student){
+        if(assignedStudents.size()<maxStudents){
+            assignedStudents.add(student);
+            return true;
+        }
+        return false;
+    }
+
+    public void setIndex(int id) {
+        this.id = id;
+    }
+
+    public int getIndex() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public static List<Destination> getDefaultDestinations() {
+        List<Destination> destinations = new ArrayList<>();
+
+        destinations.add(new Destination(1,"Utrecht University", 4));
+        destinations.add(new Destination(2,"Edinburgh University", 4));
+        destinations.add(new Destination(3,"Imperial College London", 4));
+        destinations.add(new Destination(4,"Arizona State University", 4));
+        destinations.add(new Destination(5,"Marshall University", 4));
+        destinations.add(new Destination(6,"Western Washington University", 4));
+        destinations.add(new Destination(7,"Princeton University", 4));
+        destinations.add(new Destination(8,"Cornell University", 4));
+        destinations.add(new Destination(9,"University of Pennsylvania", 4));
+        destinations.add(new Destination(10,"Stanford University", 4));
+
+        return destinations;
+    }
+    boolean isFull(){
+        return assignedStudents.size() == maxStudents;
+    }
+
+    public void setAssignedStudents(List<Student> assignedStudents) {
+        this.assignedStudents = assignedStudents;
     }
 
     public String getName() {
         return name;
     }
 
+    public int getMaxStudents() {
+        return maxStudents;
+    }
+
+    public List<Student> getAssignedStudents() {
+        return assignedStudents;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
 
-    public int getMaxCapacity() {
-        return maxCapacity;
+    public void setMaxStudents(int maxStudents) {
+        this.maxStudents = maxStudents;
     }
 
-    public void setMaxCapacity(int maxCapacity) {
-        this.maxCapacity = maxCapacity;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Destination other = (Destination) obj;
+        return Objects.equals(name, other.name);
     }
 }
